2dba6e5c2f29735915a1e4d07a4ecb589f2f09b3
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2024-10-10 13:57:15 +0100

    Adding API automation test to create new users

---
+++ cypress/e2e/apiTest.cy.js
@@ -0,0 +1,27 @@
+// cypress/integration/apiTest.cy.js
+describe('Reqres API User Creation Test', () => {
+    const testData = [
+        { "name": "admin", "job": "admin" },
+        { "name": "ronaldo", "job": "player" }
+    ];
+
+    testData.forEach(data => {
+        it(`should create user ${data.name} and validate response`, () => {
+            cy.request('POST', 'https://reqres.in/api/users', data)
+                .then((response) => {
+                    // Assert response code
+                    expect(response.status).to.equal(201);
+
+                    // Assert response schema
+                    expect(response.body).to.have.property('name', data.name);
+                    expect(response.body).to.have.property('job', data.job);
+
+                    // Assert response time
+                    expect(response.duration).to.be.lessThan(2000);
+
+                    // Assert response headers
+                    expect(response.headers['content-type']).to.include('application/json');
+                });
+        });
+    });
+});
